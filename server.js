1ed8f6bdecb8b69b695d655284c92d2975090524
Er.Mayank Pandey <mp04042007@gmail.com>
2026-04-19 18:43:47 +0530

    cloudanary added

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
+      "http://localhost:5176",
       "https://e-commerce-admin-panel-sparrow-cart.vercel.app",
     ],
     credentials: true,
